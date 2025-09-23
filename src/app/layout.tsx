93aec2fedeeba8321bab703cba3d143116fbddbb
Shaheem <shanofficial2000@gmail.com>
2025-09-22 22:00:37 -0400

    Remove unused documents and update layout structure; implement new single page component with navigation, sections for projects, experience, education, skills, testimonials, and contact form.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,9 +90,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <div className="flex min-h-screen flex-col">
-            <Navbar />
+            {/* <Navbar /> */}
             <main className="flex-1">{children}</main>
-            <Footer />
+            {/* <Footer /> */}
           </div>
         </ThemeProvider>
         <Analytics />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
 // src/app/page.tsx
 
-import AboutSection from "@/components/homePage/AboutSection";
-import CTASection from "@/components/homePage/CTASection";
-import FeaturedProjectsSection from "@/components/homePage/FeaturedProjectsSection";
-import HeroSection from "@/components/homePage/HeroSection";
-import TestimonialsSection from "@/components/homePage/TestimonialsSection";
+import SinglePage from "./single/page";
 
 export default function Home() {
-  return (
-    <>
-      <HeroSection />
+	return (
+		<>
+			<SinglePage />
+			{/* <HeroSection />
       <AboutSection />
       <FeaturedProjectsSection />
       <TestimonialsSection />
-      <CTASection />
-    </>
-  );
+      <CTASection /> */}
+		</>
+	);
 }
